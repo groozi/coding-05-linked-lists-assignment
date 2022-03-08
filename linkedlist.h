5da695ff5ee4420785514b49ef034fd0937093c7
Graziella Buitron <graziella.buitron@g.austincc.edu>
2022-03-08 01:57:29 -0600

    added comment headers to linklist.h and linkedlist.cpp

--- linkedlist.h
+++ linkedlist.h
@@ -1,3 +1,5 @@
-/*
- * your comment header here
- */
+/**********************************************************
+ * Name: Graziella Buitron
+ * Coding 05
+ * Purpose: To create a fully functional doubly linked list
+ *********************************************************/
